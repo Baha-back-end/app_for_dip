11f9c8fbeb9b665ac7b026833b8c6d87f4badd14
Baha-back-end <fancybax@gmail.com>
2024-03-21 02:46:37 +0500

    devloped DB and admin panel

--- app_for_dip/admin.py
+++ app_for_dip/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'parameters', 'price', 'reviews', 'added_at', 'updated_at')
+    list_filter = ('category', 'added_at')
+    readonly_fields = ('revews',)
+
+
+admin.site.register(Category)
+admin.site.register(Product, ArticleAdmin)
+
+
--- app_for_dip/models.py
+++ app_for_dip/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to="products/",
+                              null=True, blank=True)
+    parameters = models.TextField(default='', null=True)
+    price = models.FloatField(max_length=12, null=True)
+    reviews = models.TextField(default='', null=True)
+    added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+
+
+
